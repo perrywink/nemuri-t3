16f0d044914785c5c6f7efa60a6bb8777aaa68fe
YiJieChuah <s3847905@student.rmit.edu.au>
2023-11-08 15:47:18 +1100

    Added noto serif font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,19 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import { Noto_Serif } from 'next/font/google'
+
+//👇 Configure our font object
+const notoSerif = Noto_Serif({
+  subsets: ['latin'],
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={notoSerif.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
